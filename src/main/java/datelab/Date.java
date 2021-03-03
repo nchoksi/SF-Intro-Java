0c00e563e111cc2cede224b5ac2dd9920252f77c
simon <simon@dancingcloudservices.com>
2021-03-03 10:59:19 -0700

    Day three, 11:00 Mountain

--- src/main/java/datelab/Date.java
+++ src/main/java/datelab/Date.java
@@ -1,8 +1,28 @@
 package datelab;
 
 public class Date {
-//      - monthName
-//  - dayName
+  private int day;
+  private int month;
+  private int year;
+
+  // legal but probly not nice style
+//  private int a = 3, b, c = 99;
+
+  public Date(int day, int month, int year) {
+    if (validDate(day, month, year)) {
+      this.day = day;
+      this.month = month;
+      this.year = year;
+    } else {
+      throw new IllegalArgumentException("Bad values for a date");
+    }
+  }
+
+  public static boolean validDate(int day, int month, int year) {
+    return (month >= 1 && month <= 12
+        && day >= 1 && day <= daysInMonth(month, year));
+  }
+
   public static boolean isLeapYear(int year) {
     return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
   }
@@ -17,6 +37,7 @@ public class Date {
       return dayCounts[month - 1];
     }
   }
+
   private static final String [] monthNames = {
       "January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December",
@@ -25,5 +46,38 @@ public class Date {
     return monthNames[month - 1];
   }
 
+  private static final String [] dayNames = {
+      "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+  };
+  public static String dayName(int day) {
+    return dayNames[day];
+  }
+
+  public String asText(Date this) {
+    return "Date: " + this.day + "/" + this.month + "/" + this.year;
+  }
+
+  public int dayOfWeek(Date this) {
+    int month = this.month;
+    int year = this.year;
+    if (month < 3) {
+      month += 12;
+      year -= 1;
+    }
+    return (this.day + (13 * (month + 1) / 5) + year + year / 4 - year / 100 + year / 400) % 7;
+  }
+
+  public void tomorrow(Date this) {
+    this.day++;
+    if (this.day > daysInMonth(this.month, this.year)) {
+      this.day = 1;
+      this.month++;
+      if (this.month > 12) {
+        this.month = 1;
+        this.year++;
+      }
+    }
+  }
+  // Equality test!!!
 
 }
--- src/test/java/datelab/TestMyDate.java
+++ src/test/java/datelab/TestMyDate.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 public class TestMyDate {
   @Test
   public void test2000isALeapYear() {
+    // General format of a good Unit (or many other) test is:
+    // Setup like this    Given:
+    // When I do this...  When:
+    // this is the result Then:
+    // Tests in this form are "documentation that cannot be wrong"!!!
     assertTrue("2000 should be a leap year", Date.isLeapYear(2000));
   }
 
@@ -14,5 +19,38 @@ public class TestMyDate {
     // no setup for static methods :(
     String monthName = Date.monthName(3);
     assertEquals("Month 3 should be March", "March", monthName);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void badDateIsRejected() {
+    new Date(31, 2, 2000);
+  }
+
+  @Test
+  public void goodDateIsAccepted() {
+    new Date(29, 2, 2000);
+  }
+
+  @Test
+  public void asTextCreatesExpected() {
+    Date d = new Date(29, 2, 2000);
+    String asText = d.asText();
+    assertEquals("Feb 29, 2000 should look like Date: 2/29/2000", "Date: 29/2/2000", asText);
+  }
+
+  @Test
+  public void jan1_2000IsSaturday() {
+    Date d = new Date(1, 1, 2000);
+    int dayNum = d.dayOfWeek();
+    assertEquals("should be day zero", 0, dayNum);
+  }
+
+  @Test
+  public void tomorrowFromJan5() {
+    Date d = new Date(5, 1, 2000);
+    d.tomorrow();
+    Date expected = new Date(6, 1, 2000);
+    System.out.println("d is " + d.asText());
+    assertEquals("Date should be 6, 1, 2000", expected, d);
   }
 }
